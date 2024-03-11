94c682aa729233f755deb77cd6a4d7e8eca66bfa
juditanita <101408621+juditanita@users.noreply.github.com>
2024-03-11 15:50:47 +0100

    add style

--- script.js
+++ script.js
@@ -28,8 +28,33 @@ navLink.forEach(n=>n.addEventListener('click',linkAction));
 
 for (let i=0; i<navLink.length; i++){
     navLink[i].addEventListener('click',function(){
+      
         let current=document.getElementsByClassName('active-link');
+
+        console.log(scrollY);
         current[0].className=current[0].className.replace("active-link","");
+      
+      
+
         this.className +=" active-link";
+    
+        
+        
     })
 }
+
+
+/*==========FOOTER YEAR=======*/ 
+const yearToday= new Date().getFullYear();
+
+const footerData=document.getElementById('footerData');
+// footerData.innerText= yearToday+ ".    " + " All rigths reserved";
+footerData.innerText=`${yearToday}  All rights reserved`;
+/*=============== SHOW SCROLL UP ===============*/ 
+// function scrollUp(){
+//     const scrollUp = document.getElementById('scroll-up');
+//     // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+//     if(this.scrollY >= 400) {scrollUp.classList.add('show-scroll')}
+    
+//     else {scrollUp.classList.remove('show-scroll')}
+//  } window.addEventListener('scroll', scrollUp)
